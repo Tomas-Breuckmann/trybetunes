9cead1cd2731c27b7f93bc7670a3152f9cc31054
Tomas Breuckmann <tomas.yoga@yahoo.com>
2022-09-19 10:51:33 -0300

    light mode inicicando arquivo

--- src/Styles/light.js
+++ src/Styles/light.js
@@ -1,7 +1,17 @@
 export default {
   title: 'light',
+  general: {
+    background: 'var(--g12)',
+    color: 'var(--g3)',
+  },
+  button: {
+    // background: 'var(--b1)',
+    background: 'linear-gradient(to bottom right, var(--b10), var(--b8))',
+    color: 'var(--b1)',
+    border: 'var(--b8)',
+  },
   login: {
-    background: 'var(--g2)',
-    color: 'var(--g13)',
+    background: 'var(--g12)',
+    color: 'var(--g3)',
   },
 };
